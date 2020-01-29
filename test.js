4ea28c80710133c2ac85da75dfb7371dffec84aa
tasneembhiri <141059@ppu.edu.ps>
2020-01-29 10:05:09 +0200

    testing delete function

--- test.js
+++ test.js
@@ -1,7 +1,35 @@
 var test = require('tape');
-var logic = require('./logic');
+var logic = require('./logic.js');
 
-test('Example test', function(t) {
-  t.pass();
+
+
+
+
+
+
+test('delete testing', function(t) {
+  let array =[
+    {id:0 , description:"sports",done:false },
+    {id:1 , description:"wash",done:false},
+    
+  ];
+  
+  let actual = logic.deleteTodo(array, 0).length;
+  let expected = array.length - 1;
+  console.log("actual", actual);
+  console.log("expected", expected);
+  t.equal(actual, expected, "empty array"); 
   t.end();
 });
+
+
+
+// test('addTodo a new todo item to model.todos Array ', function (t) {
+//   // const root = document.getElementById(idList);
+//   // let li = document.createElement('li');
+//   // li.id = 'task1';
+//   // root.appendChild(li);
+//   t.deepEqual(addTodo(todos, newTodo), [-3,'first todo'], "Todo list item added.");
+//   t.end();
+// });
+
